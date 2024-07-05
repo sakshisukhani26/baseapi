bcfbf41ccbbefeafb7ed0ea932d98a4c1f415852
HP <sakshisukhani26@gmail.com>
2024-07-05 13:30:22 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-const connection=mongoose.connect('').then((value)=>{
+const url=process.env.MONGODB_URL;
+const connection=mongoose.connect('mongodb://localhost:27017/').then((value)=>{
     console.log("Db is Connected!!");
 });
 
--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -11,4 +11,4 @@ const userSchema=new mongoose.Schema({
 });
 
 const User=mongoose.model('users',userSchema);
-module.exports=User;
+module.exports=User;    
